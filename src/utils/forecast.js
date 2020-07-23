95ba591cf35233aa44f19a0279d698a0760efee4
ButterLux <52717658+ButterLux@users.noreply.github.com>
2020-07-22 22:46:30 -0500

    Setup app for HEROKU

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
-const message = 'The weather is ' + body.current.weather_descriptions[0] +'. \nIt is currently ' + body.current.temperature + 
-'F degrees. It is feels like ' + body.current.feelslike + 'F degrees outside. The humidity is' + body.current.humidity + '%'
+//const message = 'The weather is ' + body.current.weather_descriptions[0] +'. \nIt is currently ' + body.current.temperature + 
+//'F degrees. It is feels like ' + body.current.feelslike + 'F degrees outside. The humidity is' + body.current.humidity + '%'
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=e07e019bfa4c7a93a336f00d8eaf8221&query=' + latitude + ',' + longitude + '&units=f'
@@ -12,7 +12,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback ('Unable to find location', undefined)
         } else {
-            callback(undefined, message)
+            callback(undefined, 'The weather is ' + body.current.weather_descriptions[0] +'. It is currently ' + body.current.temperature + 
+            'F degrees. It is feels like ' + body.current.feelslike + 'F degrees outside. The humidity is ' + body.current.humidity + '%')
         }
     })
 }
